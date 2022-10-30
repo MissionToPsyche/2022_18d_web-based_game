913b15b88403eb6f64dbd8daf0072641664d1150
hwang2200 <94589723+hwang2200@users.noreply.github.com>
2022-10-29 19:25:55 -0700

    Update RandomizeLocation.cs

--- RandomizeLocation.cs
+++ RandomizeLocation.cs
@@ -1,3 +1,5 @@
+// WORK IN PROGRESS
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,8 +11,10 @@ public class RandomizeLocation : MonoBehaviour
     public float yRange = 3f;
 
     // Start is called before the first frame update
+    // want to randomize locations of each object populated on the screen as it scrolls through
     void Start()
     {
+        // getting range of possible positions for object to spawn
         float xPosition = Random.Range(0 - xRange, 0 + xRange);
         float yPosition = Random.Range(0 - yRange, 0 + yRange);
         randomPosition = new Vector2(xPosition, yPosition);
