6effd8dea11bda9577340735c0fbe0d98b49e64f
hwang2200 <94589723+hwang2200@users.noreply.github.com>
2022-11-03 15:35:53 -0700

    Add files via upload

---
+++ InitialBackground.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialBackground : MonoBehaviour
+{
+    public BoxCollider2D boxCollider;
+    public Rigidbody2D rigidBody;
+    private float imageWidth;
+    private float scrollSpeed = -10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        imageWidth = boxCollider.size.x;
+        boxCollider.enabled = false;        //save memory, store reference to collider in imageWidth
+
+        rigidBody.velocity = new Vector2(scrollSpeed, 0);       //scroll to left
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
