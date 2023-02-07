824c6e10c8be66a7b8dc90783891986e11c7f62f
hwang2200 <94589723+hwang2200@users.noreply.github.com>
2023-02-07 01:21:58 -0700

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     //Movement speeds
-    public float defaultSpeed = 9.0f;
+    public float defaultSpeed = 2f;
+    private Animator animation;
+    private Rigidbody2D rb;
+    //public Sprite upSprite, downSprite;
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        animation = GetComponent<Animator>();
     }
 
     void Update()
     {
         Vector3 pos = transform.position;
+
         pos.x += defaultSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        pos.y += defaultSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
         transform.position = pos;
 
-        pos.y += defaultSpeed * Time.deltaTime * Input.GetAxis("Vertical");
-        transform.position = pos;
+        if(rb.velocity.y == 0)
+        {
+            animation.SetBool("flyingUp", false);
+            animation.SetBool("flyingDown", false);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            animation.SetBool("flyingUp",  true);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            animation.SetBool("flyingDown",  true);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D obj)
